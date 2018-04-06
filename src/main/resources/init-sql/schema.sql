44979cffae503225c2c989ac1ba6520555028031
971005287@qq.com <G278363604>
2018-04-07 04:09:58 +0800

    增加初始化数据库脚本

---
+++ src/main/resources/init-sql/schema.sql
@@ -0,0 +1,38 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : 腾讯云GW
+ Source Server Type    : MySQL
+ Source Server Version : 80004
+ Source Host           : 140.143.237.60:3306
+ Source Schema         : alibb
+
+ Target Server Type    : MySQL
+ Target Server Version : 80004
+ File Encoding         : 65001
+
+ Date: 07/04/2018 04:09:01
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for ali_subject_record
+-- ----------------------------
+DROP TABLE IF EXISTS `ali_subject_record`;
+CREATE TABLE `ali_subject_record`  (
+  `id` char(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '姓名',
+  `phone` char(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '电话',
+  `sex` char(4) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '性别',
+  `age` int(3) NULL DEFAULT NULL COMMENT '年龄',
+  `type` char(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '题型',
+  `result` json NULL COMMENT '答题结果',
+  `create_time` datetime(0) NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `ip` char(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT 'IP地址',
+  `browser_info` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL COMMENT '浏览器信息',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '答题记录' ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
