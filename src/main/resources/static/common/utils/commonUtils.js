a265da9aca0db3400808bab582a6de18b2da4f35
971005287@qq.com <971005287@qq.com>
2018-04-11 17:36:13 +0800

    增加登陆功能（前端简易验证）

--- src/main/resources/static/common/utils/commonUtils.js
+++ src/main/resources/static/common/utils/commonUtils.js
@@ -50,3 +50,34 @@ function IFNULL(str, defStr){
      }
      return str;
 }
+
+
+
+
+
+
+
+//设置cookie
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + exdays);
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + "; " + expires;
+}
+//获取cookie
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1);
+        if (c.indexOf(name) != -1) return c.substring(name.length, c.length);
+    }
+    return "";
+}
+//清除cookie
+function clearCookie(name) {
+    setCookie(name, "", -1);
+}
+
+
