e5259470b4378bc6ee795ed01c07434781aa7574
971005287@qq.com <971005287@qq.com>
2018-05-02 13:41:47 +0800

    区分HOST与APIHOST

--- src/main/resources/static/common/utils/commonConfig.js
+++ src/main/resources/static/common/utils/commonConfig.js
@@ -3,7 +3,8 @@
  * @type {{host: string}}
  */
 var Config = {
-     host:'https://api.alibbb.com.cn/'
+     host:'https://api.alibbb.com.cn/',
+     ApiHost:'https://api.alibbb.com.cn/',
      // host:'http://140.143.237.60:8010/'
     // host:'http://127.0.0.1:8010/'
 }
